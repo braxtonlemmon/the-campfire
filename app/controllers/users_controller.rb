b75463b1f9b4d11879d3fa3cb8cf3c0eb987de70
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-10-18 11:27:26 +0200

    Make activity notifications refer to current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
-
 	def show
 		@post = Post.new
 		@user = User.find(params[:id])
 		@posts = @user.feed.paginate(page: params[:page])
-	end
-
-	def friends
-		@user = User.find(params[:id])
 	end
 
 	def index
