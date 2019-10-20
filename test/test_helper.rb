8d871152243c9c0abb50bd28b4bb4866300d3a73
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-10-20 10:44:51 +0200

    Add counter for 'likes'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,10 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+	fixtures :all
+	
+	# allows parallel testing...
+		# parallelize(workers: 2)
 
   # Add more helper methods to be used by all tests here...
 end
