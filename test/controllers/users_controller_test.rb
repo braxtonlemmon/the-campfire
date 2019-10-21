150b4b33d15ae1088536de6ffa0feba1e5620f5a
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-10-21 07:50:24 +0200

    Write users_controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,13 +8,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		@other_user = users(:greg)
 	end
 
-	# test show
 	test 'should redirect show when not logged in' do
 		get user_path @user
 		assert_redirected_to new_user_session_path
 	end
 
-  # test 
+	test 'should show user page when logged in' do
+		sign_in @user
+		get user_path @other_user
+		assert_response :success
+	end
+ 
 	test 'should redirect index when not logged in' do
 		get users_path
 		assert_redirected_to new_user_session_path
@@ -37,12 +41,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 		assert_redirected_to root_path
 	end
 
-	
-
 	test 'should redirect update when not logged in' do
 		patch user_path(@user), params: { user: { name:  @user.name,
 																							email: @user.email } }
 		assert_redirected_to new_user_session_path
-	end																							
-
+	end		
+	
+	test 'should redirect update when logged in as wrong user' do
+		sign_in @user
+		patch user_path(@other_user), params: { user: { name:  @user.name,
+																										email: @user.email } }
+		assert_redirected_to root_path																										
+	end
 end
