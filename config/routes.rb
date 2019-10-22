e61adc7653e8bb5449a47104942ca406a94e27d5
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-10-22 14:46:26 +0200

    Modify routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 	root 'static_pages#home'
 	devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-																	
+
+
 	resources :users, only: [:show, :index] do
 		get 'friends', on: :member
 	end
@@ -12,7 +13,6 @@ Rails.application.routes.draw do
 	resources :posts, only: [:create, :destroy]
 	resources :likes
 	resources :comments
-	resources :images
 	resources :activities
 	mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
