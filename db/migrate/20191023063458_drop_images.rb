49e67bb95eb76953d54d964bc126dd6947bc8aad
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-10-23 08:48:36 +0200

    Revert "Drop images table"
    This reverts commit f81b81b05e82b693693696b04ee041d6ce85bf6b.

--- db/migrate/20191023063458_drop_images.rb
+++
@@ -1,5 +0,0 @@
-class DropImages < ActiveRecord::Migration[5.2]
-	def change
-		drop_table :images
-  end
-end
---
+++ test/controllers/images_controller_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ImagesControllerTest < ActionDispatch::IntegrationTest
+end
